22b5b911242d4c53f4efd3d105d59c5ff7ae8c44
Harsh Gautam <122910800+Harsh12345678999@users.noreply.github.com>
2023-10-18 14:45:48 +0530

    testing

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
+const express = require("express");
+const app = express();
+app.use(express.json());
 
+// import router
+const product = require("./routers/productRouter");
+app.use("/api/v1", product);
 
-//import router
-const product = require("./routers/productRouter")
-app.use("/api/v1", product)
+// middleware error
+const errorMiddleware = require("./middleware/error");
+app.use(errorMiddleware);
 
-//middleware error
-const errorMiddleware = require("./middleware/error")
-app.use(errorMiddleware)
+// default route
+app.get("/", (req, res) => {
+  res.status(200).json({ message: 'Hello from the backend!' });
+});
 
-module.exports = app
+module.exports = app;
